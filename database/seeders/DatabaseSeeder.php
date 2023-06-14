f35dafd9ddd0bbcfc48d7f0618499db6497a8662
elmu53b <musabgaili@gmail.com>
2023-06-14 23:30:45 +0300

    adding admins seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,68 +18,63 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        // User::create([
+        //     'name'=> 'musab',
+        //     'city_id'=> 2,
+        //     'password'=> Hash::make('123456'),
+        //     'type'=> 'admin',
+        //     'address'=> 'saudi arabia',
+        //     'hood'=> 'not-required',
+        //     'phone'=> '0919232991',
+        //     'wa'=> '0919232991',
         // ]);
-        // City::create(
-        //     ['name' => 'دنقلا'],
-        // );
-        City::create(
-            [
-                'name' => 'الخرطوم'
-            ]
-        );
-        City::create(
-            [
-                'name' => 'ام درمان'
-            ]
-        );
+        // User::create([
+        //     'name'=> 'Abdalla',
+        //     'city_id'=> 2,
+        //     'password'=> Hash::make('123456'),
+        //     'type'=> 'admin',
+        //     'address'=> '--',
+        //     'hood'=> '--',
+        //     'phone'=> '0121941942',
+        //     'wa'=> '0121941942',
+        // ]);
 
-        City::create(
-            [
-                'name' => 'بحري'
-            ]
-        );
+
+
         User::create([
-            'name'=> 'musab',
-            'city_id'=> 2,
-            'password'=> Hash::make('123456'),
-            'type'=> 'admin',
-            'address'=> 'saudi arabia',
-            'hood'=> 'not-required',
-            'phone'=> '0919232991',
-            'wa'=> '0919232991',
-        ]);
-        User::create([
-            'name'=> 'Abdalla',
-            'city_id'=> 2,
-            'password'=> Hash::make('123456'),
+            'name'=> 'نادين',
+            'city_id'=> 1,
+            'password'=> Hash::make('N0911493546'),
             'type'=> 'admin',
             'address'=> '--',
             'hood'=> '--',
-            'phone'=> '0121941942',
-            'wa'=> '0121941942',
+            'phone'=> '0911493546',
+            'wa'=> '0911493546',
         ]);
 
-        Category::create([
-            'name'=> "Cat 1",
-            "desc" => "Heart Disesase , "
-        ]);
-        Medicine::create([
-            'category_id'=> 1,
+        User::create([
+            'name'=> 'نجبل',
             'city_id'=> 1,
-            'user_id'=> 1,
-            'name' => 'name',
-            'price_type' => 'not_free',
-            'dose' => 'dose',
-            'name_en' => 'name_en',
-            'ex_date' => 'ex_date',
-            'tags' => 'tags',
-            'quantity' => 'quantity'
+            'password'=> Hash::make('N0119114568'),
+            'type'=> 'admin',
+            'address'=> '--',
+            'hood'=> '--',
+            'phone'=> '0119114568',
+            'wa'=> '0119114568',
         ]);
+
+        User::create([
+            'name'=> 'مختار',
+            'city_id'=> 1,
+            'password'=> Hash::make('M0126949107'),
+            'type'=> 'admin',
+            'address'=> '--',
+            'hood'=> '--',
+            'phone'=> '0126949107',
+            'wa'=> '0126949107',
+        ]);
+
 
         // HERE IT
     }
